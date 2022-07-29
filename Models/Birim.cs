2914778f1243a849948418e09c345a22dcf306c8
bayokuzz <patatesadam91@gmail.com>
2022-07-29 10:48:23 +0300

    Initial commit

---
+++ Models/Birim.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjeCore.Models
+{
+    public class Birim
+    {
+        [Key]
+        public int BirimID { get; set; }
+        public string BirimAd { get; set; }
+        public IList<Personel> Personels { get; set; }
+    }
+}
---
+++ Models/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjeCore.Models
+{
+    public class Context: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-G35LKSK\\SQLEXPRESS;" +
+                "database=BirimDB; integrated security=true;");
+        }
+
+        public DbSet<Birim> Birims { get; set; }
+        public DbSet<Personel> Personels { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+    }
+}
